6096f54317b3e80762bce6e91333569a1aaaa0d8
maryqu3en <123774566+maryqu3en@users.noreply.github.com>
2023-05-09 23:51:14 +0100

    Add files via upload

---
+++ deleteEvenNodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct Node
+{
+    int data;
+    node * next;
+}node;
+
+//nothing more fun than dealing with linked lists
+
+node * deletePosition(node * head, int pos) {
+    node * temp= head;
+    for (int i = 1; i<pos; i++) {
+        temp = temp->next;
+    }
+    node * t = temp->next;
+    temp->next = t->next;
+    free(t);
+    return head;
+}
+
+node * deleteEven (node * head)  {
+    node * temp = head;
+    int i=1;
+    while (temp != NULL)
+    {
+        node * t = temp;
+        temp = temp->next;
+        if (t->data % 2 == 0){
+            head = deletePosition(head, i);
+        }
+        i++;
+    }
+    return head;
+}
